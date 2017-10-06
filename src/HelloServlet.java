313dbde1a193384a4cce339e84d6f84c48887035
xiangxing <xiangxing@192.168.1.103>
2017-10-06 11:16:41 +0800

    test

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response){
 
         try {
-            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println("<h1>Hello Servlet HaHa!</h1>");
             response.getWriter().println(new Date().toLocaleString());
         } catch (IOException e) {
             // TODO Auto-generated catch block
